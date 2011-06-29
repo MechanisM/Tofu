276fb30a95d8fd4962f1a8eb470e99cc3197ca0d
Alessandro Ghedini <al3xbio@gmail.com>
2011-06-29 21:50:34 +0200

    add tofu_status() to rep.h

--- include/tofu/rep.h
+++ include/tofu/rep.h
@@ -44,6 +44,7 @@ typedef struct {
 tofu_rep_t *tofu_rep_init();
 void        tofu_rep_free(tofu_rep_t *rep);
 
+void tofu_status(tofu_rep_t *rep, int status);
 void tofu_write(tofu_rep_t *rep, const char *s);
 void tofu_writef(tofu_rep_t *rep, const char *fmt, ...);
 void tofu_head(tofu_rep_t *rep, const char *field, const char *value);
